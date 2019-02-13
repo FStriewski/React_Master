776bace19210b65ace71b0f01bb0a26d00a7cfdd
FS <fstriewski@outlook.com>
2019-02-13 22:55:15 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+123
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/RenderProps/LogicComponent.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const LogicComponent = () => <div>LogicComponent</div>
