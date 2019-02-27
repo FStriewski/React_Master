a3799a99b3c2c5f61f0c8bab10aa84ab5860d3ca
FS <fstriewski@outlook.com>
2019-02-27 22:30:28 +0100

    Add context

---
+++ src/Context/ContextProvider.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export class ContextProvider extends React.Component<{},{}>{
+    state = {
+
+        
+    }
+
+
+    render(){
+        return(
+            <React.Fragment>
+                123
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/Context/IntermediateComponents.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+export const Component1 = () => <div>Something</div>
+export const Component2 = () => <div>Something</div>
+export const Component3 = () => <div>Something</div>
+export const Component4 = () => <div>Something</div>
