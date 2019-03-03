ab4bac654114ddbdee84ba6f481b21e0c42b74bf
FS <fstriewski@outlook.com>
2019-03-03 21:49:42 +0100

    Update style

--- src/Hooks/Component.tsx
+++ src/Hooks/Component.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Props } from '../types';
+import { Style } from '../styles/style';
 
 export const Component = ({ count, name, countUp }: Props) => (
-    <React.Fragment>
+    <Style>
         <div>This is a react hook child named {name} and count {count}</div>
         <button onClick={countUp}>Count up</button>
-    </React.Fragment>
+    </Style>
 );
